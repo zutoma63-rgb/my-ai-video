24ab49b9e67928b00d02f5c314e52e4fba1903e5
zutoma63-rgb <zutoma63@gmail.com>
2026-06-12 18:21:19 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -7,4 +7,18 @@ function generateVideo() {
   alert("動画生成開始！");
 }
 alert("script.js 読み込み成功！");
+const imageInput = document.getElementById("imageInput");
+const preview = document.getElementById("preview");
+
+imageInput.addEventListener("change", (event) => {
+    const file = event.target.files[0];
+
+    if (file) {
+        preview.src = URL.createObjectURL(file);
+    }
+});
+
+document.getElementById("generateBtn").addEventListener("click", () => {
+    alert("動画生成開始！");
+});
 </script>
